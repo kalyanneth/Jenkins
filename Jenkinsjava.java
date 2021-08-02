627b58ec22a232fa1e60b02eea33714551ad9b6f
kalyanneth <85828955+kalyanneth@users.noreply.github.com>
2021-08-02 19:40:41 +0530

    Update Jenkinsjava.java

--- Jenkinsjava.java
+++ Jenkinsjava.java
@@ -2,7 +2,7 @@ public class Jenkinsjava
 {
 	public static void main(String[] args)
 	{
-		System.out.println("*****Hello World To Jenkins Program made few changes****");
+		System.out.println("*****Hello World To Jenkins Program made few changes second time****");
 	}
 
 }
